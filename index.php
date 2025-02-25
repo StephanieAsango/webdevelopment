8dff9b11954599ce5ed1ba1da2279a7e569fc53d
StephanieAsango <asangostephanie14@gmail.com>
2025-02-25 16:18:44 +0300

    Second Commit: Introduction to PHP

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// This tells the server that PHP code begins here
+//Everything between <?php and ? > is processed as PHP code 
+
+// echo is a language construct that outputs text/HTML
+
+echo "Hello, World!";
+//This marks the end of PHP code 
+?>
---
+++ intro.php
@@ -0,0 +1,34 @@
+Introduction to PHP
+What is PHP 
+PHP(PHP: Hypertext Processer) is a server-side scripting language designed specifically for web development.
+To understand PHP's role let's first understand how websites work:
+
+1: When you visit a website , your browser(the client) sends a request to a web server
+2: The server processes this request
+3: For static websites, the simply returns HTML files
+4. For dynamic websites using PHP, the server:
+    -Executes the PHP code 
+    -Generates HTML based on codes output 
+    -Sends the HTML back to the browser 
+
+Think of it like ordering at a restaurant
+-You the (browser) make an order(request)
+-The chef (PHP) prepares the meal(process the code)
+-The waiter (server) brings you the finished meal(HTML)
+
+Settinh up your development enviroment
+To write PHP code, you need:
+1. A web server(Apache)
+2. PHP interpreter
+3.Database server (MySQL)
+4. Text editor or IDE
+
+The easiest way to get started is by installing XAMPP, which includes all these components.
+
+My first script 
+Lets write and understand my first script 
+
+<?php
+
+?>
+
