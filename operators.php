9023cbe385cb75f81fe7adec81c5e92b1ad06160
StephanieAsango <asangostephanie14@gmail.com>
2025-02-26 16:35:47 +0300

    Update operators.php

---
+++ operators.php
@@ -0,0 +1,51 @@
+<?php
+// PHP Operators
+
+// Operators in PHP allow you to perform operations on variable sand values. They are fundamental building blocks for creating expressions that manipulate data in your programs.
+
+// 1. Arithmetic Operators
+// These operators perform basic mathematical operations
+
+// Addition (+)
+// The addition operator adds two values together
+$sum = 10+5; //Result will be 15
+echo "Sum : $sum <br>";
+
+// Subtraction (-)
+// The subtraction operation subtracts the right operands from the left operand
+$diffrence = 10-5; //Result will be 5
+echo "diffrence : $diffrence <br>";
+
+// Multiplication(*)
+// Multiplies two values
+$product = 10*5; //Result will be 50
+echo "product : $product<br>";
+
+// Division (/)
+// divides the left operand by the right operand
+// Note division by zero will cause a fatal error
+$quotient = 10/5; //Result will be 2
+echo "Quotient : $quotient <br>";
+
+// Modulus (%)
+// The modulus operator returns the remainder of a division
+// 10 divided by 3 equals 3 with a remainder of 1
+$remainder = 10 %3; //Result will be 1
+echo "remainder : $remainder <br>";
+
+// Exponentiation(**)
+// Raises the left operand to the power of the right operand 
+// 2 raised to the power of 3 is 8
+$power = 2**3; //Result will be 8
+echo "power : $power <br>";
+
+// Integer Division (intdiv())
+// Returns the integer quotient of the division 
+// 10 divided by 3 gives 3 (truncates the decimal part)
+$intDivision = intdiv(10, 3); //Result will be 3
+echo "Integer Division:$intDivision <br>";
+
+// Tomorrow - Assignment operators
+
+
+?>
