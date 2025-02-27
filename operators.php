23284eb93e492ba8b8ea4e091de150717d12dae6
StephanieAsango <asangostephanie14@gmail.com>
2025-02-27 16:37:02 +0300

    Update operators.php

--- operators.php
+++ operators.php
@@ -6,6 +6,7 @@
 // 1. Arithmetic Operators
 // These operators perform basic mathematical operations
 
+echo "<b>1. Arithmetic operators</b><br>";
 // Addition (+)
 // The addition operator adds two values together
 $sum = 10+5; //Result will be 15
@@ -46,6 +47,96 @@ $intDivision = intdiv(10, 3); //Result will be 3
 echo "Integer Division:$intDivision <br>";
 
 // Tomorrow - Assignment operators
+echo "<b>2. Assignment operators</b><br>";
+// These operators assign values to variables
+// Basic assignment(=)
+$x = 10;  //Assign 10 to x
+
+// Combined assignment operators
+$x = 10;
+echo "x = $x <br>";
+
+$x += 5; //Equivalent to: $x =$x + 5; result:15
+echo "x += 5 is $x <br>";
+
+$x -= 3; //Equivalent to: $x =$x -3; result:12
+echo "x -= 3 is $x <br>";
+
+$x *= 2; //Equivalent to: $x =$x *2; result:24 
+echo "x *= 2 is $x <br>";
+
+$x /= 4; //Equivalent to: $x =$x /4 ; result:6 
+echo "x /= 4 is $x <br>"; 
+
+$x %= 4; //Equivalent to: $x =$x %4 ; result:2 
+echo "x %= 4 is$x <br>";
+
+$x **=3; //Equivalent to: $x =$x **4 ; result:8
+echo "x **=3  is$x <br>";
 
 
+//  Combined assignment operators perform an operation and assignment in one step
+
+// Comparison Operators 
+echo " <b>3. Comparison Operators</b><br>";
+// These operators compare two values and return a boolean result (true or false)
+
+// Equal(==)
+echo var_dump(5==5) . "<br>"; //bool(true)
+echo var_dump(5=="5"). "<br>"; //bool(true)  values are equal after type juggling
+
+// Identical (===)
+echo var_dump(5===5). "<br>"; //bool(true)
+echo var_dump(5==="5"). "<br>"; //bool(false) checks both the value and the datatype diffrent values
+
+// Not equal (!= or <>)
+echo var_dump(5!=5). "<br>"; //bool(false)
+echo var_dump(5<>5). "<br>"; //bool(false) alternative syntax
+
+// Not identical (!==)
+echo var_dump(5!=="5"). "<br>"; //bool(true)
+
+// Greater than (>)
+echo var_dump(10>5). "<br>"; //bool(true)
+
+// Less than(>)
+echo var_dump(5<10). "<br>"; //bool(true)
+
+// Greater than or equal to(>=)
+echo var_dump(10>=10). "<br>"; //bool(true)
+
+// Less than or equal to (<=)
+echo var_dump(5<=5). "<br>"; //bool(true)
+
+// Logical operations
+echo " <b>3. Logical Operators</b><br>";
+// These operators perform logical operations on boolean values  - AND, OR, NOT, XOR
+
+// 1. AND (&&, and) - all operands must be true to have a true result
+echo " <b><i>3(i) Logical AND (&&, and)</b></i><br>";
+echo var_dump(true && true) ; //bool(true)
+echo var_dump(true && false); //bool(false)
+echo var_dump(false && true); //bool(false)
+echo var_dump(false && false); //bool(false)
+
+// 2. OR (|| , or) - atleast one of the operand must be true
+echo " <br><b><i>3(i) Logical OR (||, or)</b></i><br>";
+
+echo var_dump(true || true) ; //bool(true)
+echo var_dump(true or false) ; //bool(true)
+echo var_dump(false || true) ; //bool(true)
+echo var_dump(false or false) ; //bool(false)
+
+// 3. NOT (!, not) - reverse a condition
+echo " <br><b><i>3(i) Logical NOT (!, not)</b></i><br>";
+var_dump(!true); //bool(false)
+var_dump(!true); //bool(true)
+// var_dump(not true);
+
+// 2. XOR (xor) - exclusive OR, true if one is true, but not both
+echo " <br><b><i>3(i) Exclusive OR (!, not)</b></i><br>";
+var_dump(true xor false); //bool(true)
+var_dump(true xor true); //bool(false)
+
+// Tommorow -String operations
 ?>
