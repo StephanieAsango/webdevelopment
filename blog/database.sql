d0c8fe1190be2216893f58d2d6a6d0529895a359
StephanieAsango <asangostephanie14@gmail.com>
2025-03-24 16:16:03 +0300

    Update database.sql

--- blog/database.sql
+++ blog/database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS blog_db;
 USE blog_db;
 
 CREATE TABLE users(
-    id int AUTOINCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(200) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -11,16 +11,16 @@ CREATE TABLE users(
 );
 
 CREATE TABLE categories(
-    id int AUTOINCREMENT PRIMARY KEY,
+    id int AUTOI_NCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     slug VARCHAR(50) NOT NULL UNIQUE
     );
 
 CREATE TABLE posts(
-    id int AUTOINCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     content TEXT NOT NULL,
-    slug VARCHAR(50) NOT NULL UNIQUE
+    slug VARCHAR(50) NOT NULL UNIQUE,
     user_id INT NOT NULL,
     category_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -30,7 +30,7 @@ CREATE TABLE posts(
 );
 
 CREATE TABLE comments(
-    id int AUTOINCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     content TEXT NOT NULL,
     post_id INT NOT NULL,
     user_id INT NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE comments(
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 );
 
---Insert some initial data
+-- Insert some initial data
 INSERT INTO categories(name, slug)  VALUES
 ('Technology', 'technology'),
 ('Travel', 'travel'),
